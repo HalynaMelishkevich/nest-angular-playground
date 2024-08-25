11e5d617c168d0bd16ade6e1973f1732a1b40ae0
Halyna Melishkevych <melishkevych@contractor.aveone.com>
2024-08-25 22:16:31 +0300

    Update prod configs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.ENVIRONMENT === 'dev') {
-    app.enableCors();
-  }
+  app.enableCors({
+    origin: [
+      process.env.ENVIRONMENT === 'dev'
+        ? 'http://localhost:4200'
+        : 'https://nest-angular-playground-frontend.onrender.com',
+    ],
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
